22e0172682418f7c31952d881b09d7217e8af2c1
无敌暴龙兽 <13131622714@aliyun.com>
2024-10-09 18:51:54 +0800

    Site updated: 2024-10-09 18:51:54

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1728470994595";
+const VERSION = "1728471113308";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
